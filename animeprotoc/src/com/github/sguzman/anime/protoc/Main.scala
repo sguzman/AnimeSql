3a931fa05c4797330510b501fc897f3a174e79db
Salvador Guzman <guzmansalv@gmail.com>
2018-04-03 17:51:41 -0700

    some logging

--- animeprotoc/src/com/github/sguzman/anime/protoc/Main.scala
+++ animeprotoc/src/com/github/sguzman/anime/protoc/Main.scala
@@ -174,7 +174,9 @@ object Main {
       val file = new File("./store.msg")
       val output = new FileOutputStream(file)
 
+      scribe.info("Wrinting store.msg")
       store.StoreCache(items).writeTo(output)
+      scribe.info("Wrote store.msg")
       output.close()
     }
 

6de9cc5ff4c2843284afc8f7c6eadf0e39a5819b
Salvador Guzman <guzmansalv@gmail.com>
2018-04-10 15:30:52 -0700

    yes! - explosion!

--- animesql/src/com/github/sguzman/anime/sql/Main.scala
+++ animesql/src/com/github/sguzman/anime/sql/Main.scala
@@ -107,158 +107,163 @@ object Main {
   }
 
   def main(args: Array[String]): Unit = {
-    identity {
-      val pages = 1 to 318
-      val shows = pages.par.flatMap{a =>
-        val url = s"https://www.anime-planet.com/anime/all?page=$a"
-        val html = HUtil.retryHttpGet(url)
-        val doc = html.doc
-
-        doc.flatMap("div#siteContainer > ul.cardDeck > li.card").map{ b =>
-          val doc2 = b.innerHtml.doc
-          val img = doc2.map("a[title] > div.crop > img[src]", url).attr("src")
-          val title = doc2.map("a[title] > h4").text
-          val link = doc2.map("a[href]").attr("href")
-          val inner = doc2.map("a[title]").attr("title").doc
-
-          val `type` = inner.map("ul.entryBar > li.type").text
-          val year = inner.maybe("ul.entryBar > li.iconYear").map(_.text).getOrElse("")
-          val studio = inner.map("ul.entryBar > li:nth-child(2)").text
-          val rating = inner.maybe("ul.entryBar > li > div.ttRating").map(_.text.toDouble).getOrElse(-1.0d)
-          val desc = inner.map("p").text
-
-          val genres = inner.flatMap("div.tags > ul > li").map(_.text)
-          val fullTitle = AnimeSummary(title, img, link, desc, studio, year, rating, `type`, genres)
-          fullTitle
-        }
-      }.toList
-      itemCache = itemCache.addAllSums(shows)
-      true
-    } && identity {
-      val animes = itemCache.sums.par.map{a =>
-        val url = s"https://www.anime-planet.com${a.link}"
-
-        val anime = get(url)(itemCache.cache.contains)(itemCache.cache.apply){doc =>
-          val alt = doc.maybe("h2.aka").map(_.text).getOrElse("")
-          println(a.link)
-          val rawRank = doc.map("#siteContainer > section.pure-g.entryBar > div:nth-child(5)").text
-          val rank = if(rawRank.isEmpty) -1
-          else rawRank.after("#")
-            .replaceAll(",", "")
-            .trim
-            .toInt
-
-          val id = doc.map("""form[data-mode="anime"]""").attr("data-id").toLong
-          val u = a.link.stripPrefix("/anime/")
-
-          Anime(Some(a), alt, rank, id, u)
-        }
-
-        url -> anime
-      }.toList
-
-      itemCache = itemCache.addAllCache(animes)
-      true
-    } && identity {
-      val users = itemCache.cache.par.map{a =>
-        val url = s"https://www.anime-planet.com/ajaxDelegator.php?mode=stats&type=anime&id=${a._2.id}&url=${a._1.afterLast("/")}"
-
-        val user = get(url)(itemCache.anime.contains)(itemCache.anime.apply){doc =>
-          val watched = doc.maybe("ul.statList > li.status1 > a > span.slCount").map(_.text.replaceAll(",","").toInt).getOrElse(0)
-          val watching = doc.map("ul.statList > li.status2 > a > span.slCount").text.replaceAll(",","").toInt
-          val wantToWatch = doc.map("ul.statList > li.status3 > a > span.slCount").text.replaceAll(",","").toInt
-          val stalled = doc.map("ul.statList > li.status4 > a > span.slCount").text.replaceAll(",","").toInt
-          val dropped = doc.map("ul.statList > li.status5 > a > span.slCount").text.replaceAll(",","").toInt
-          val wontWatch = doc.map("ul.statList > li.status6 > a > span.slCount").text.replaceAll(",","").toInt
-
-          AnimeUser(Some(a._2), Some(UserStats(watched, watching, wantToWatch, stalled, dropped, wontWatch)))
-        }
-
-        url -> user
-      }.toList
-
-      itemCache = itemCache.addAllAnime(users)
-      true
-    } && identity {
-      final class Genres(tag: Tag) extends Table[(Int, String)](tag, "genres") {
-        def id = column[Int]("genre_id", O.Unique, O.PrimaryKey, O.AutoInc)
-        def name = column[String]("name")
-
-        def * = (id, name)
-      }
-      val genres = TableQuery[Genres]
-
-      final class Summary(tag: Tag) extends Table[(Int, String, String, String, String, String, Double, String)](tag, "summary") {
-        def id = column[Int]("summary_id", O.Unique, O.PrimaryKey, O.AutoInc)
-        def title = column[String]("title")
-        def img = column[String]("img")
-        def link = column[String]("link", O.Unique, O.Length(200))
-        def desc = column[String]("desc")
-        def studio = column[String]("studio")
-        def rating = column[Double]("rating")
-        def showType = column[String]("showType")
-
-        def * = (id, title, img, link, desc, studio, rating, showType)
-      }
-      val summary = TableQuery[Summary]
-
-      final class List(tag: Tag) extends Table[(Int, Int, Int)](tag, "list") {
-        def id = column[Int]("id", O.Unique, O.PrimaryKey, O.AutoInc)
-        def sumId = column[Int]("sum_id")
-        def genId = column[Int]("gen_id")
-
-        def * = (id, sumId, genId)
-      }
-      val list = TableQuery[List]
-
-      val db = Database.forURL("jdbc:postgresql://localhost:5432/postgres", driver = "org.postgresql.Driver", user = "alice", password = "pass")
-
+    println(
       identity {
-        val tables = List(genres, summary, list)
+        identity {
+          val pages = 1 to 318
+          val shows = pages.par.flatMap{a =>
+            val url = s"https://www.anime-planet.com/anime/all?page=$a"
+            val html = HUtil.retryHttpGet(url)
+            val doc = html.doc
 
-        tables.par.foreach{a =>
-          util.Try(db.run(DBIO.seq(a.schema.create)).v) match {
-            case Failure(e) => println(s"Could not create db ${e.getMessage}")
-            case _ => println("Successfully created db")
+            doc.flatMap("div#siteContainer > ul.cardDeck > li.card").map{ b =>
+              val doc2 = b.innerHtml.doc
+              val img = doc2.map("a[title] > div.crop > img[src]", url).attr("src")
+              val title = doc2.map("a[title] > h4").text
+              val link = doc2.map("a[href]").attr("href")
+              val inner = doc2.map("a[title]").attr("title").doc
+
+              val `type` = inner.map("ul.entryBar > li.type").text
+              val year = inner.maybe("ul.entryBar > li.iconYear").map(_.text).getOrElse("")
+              val studio = inner.map("ul.entryBar > li:nth-child(2)").text
+              val rating = inner.maybe("ul.entryBar > li > div.ttRating").map(_.text.toDouble).getOrElse(-1.0d)
+              val desc = inner.map("p").text
+
+              val genres = inner.flatMap("div.tags > ul > li").map(_.text)
+              val fullTitle = AnimeSummary(title, img, link, desc, studio, year, rating, `type`, genres)
+              fullTitle
+            }
+          }.toList
+          itemCache = itemCache.addAllSums(shows)
+          true
+        } && identity {
+          val animes = itemCache.sums.par.map{a =>
+            val url = s"https://www.anime-planet.com${a.link}"
+
+            val anime = get(url)(itemCache.cache.contains)(itemCache.cache.apply){doc =>
+              val alt = doc.maybe("h2.aka").map(_.text).getOrElse("")
+              println(a.link)
+              val rawRank = doc.map("#siteContainer > section.pure-g.entryBar > div:nth-child(5)").text
+              val rank = if(rawRank.isEmpty) -1
+              else rawRank.after("#")
+                .replaceAll(",", "")
+                .trim
+                .toInt
+
+              val id = doc.map("""form[data-mode="anime"]""").attr("data-id").toLong
+              val u = a.link.stripPrefix("/anime/")
+
+              Anime(Some(a), alt, rank, id, u)
+            }
+
+            url -> anime
+          }.toList
+
+          itemCache = itemCache.addAllCache(animes)
+          true
+        } && identity {
+          val users = itemCache.cache.par.map{a =>
+            val url = s"https://www.anime-planet.com/ajaxDelegator.php?mode=stats&type=anime&id=${a._2.id}&url=${a._1.afterLast("/")}"
+
+            val user = get(url)(itemCache.anime.contains)(itemCache.anime.apply){doc =>
+              val watched = doc.maybe("ul.statList > li.status1 > a > span.slCount").map(_.text.replaceAll(",","").toInt).getOrElse(0)
+              val watching = doc.map("ul.statList > li.status2 > a > span.slCount").text.replaceAll(",","").toInt
+              val wantToWatch = doc.map("ul.statList > li.status3 > a > span.slCount").text.replaceAll(",","").toInt
+              val stalled = doc.map("ul.statList > li.status4 > a > span.slCount").text.replaceAll(",","").toInt
+              val dropped = doc.map("ul.statList > li.status5 > a > span.slCount").text.replaceAll(",","").toInt
+              val wontWatch = doc.map("ul.statList > li.status6 > a > span.slCount").text.replaceAll(",","").toInt
+
+              AnimeUser(Some(a._2), Some(UserStats(watched, watching, wantToWatch, stalled, dropped, wontWatch)))
+            }
+
+            url -> user
+          }.toList
+
+          itemCache = itemCache.addAllAnime(users)
+          true
+        } && identity {
+          final class Genres(tag: Tag) extends Table[(Int, String)](tag, "genres") {
+            def id = column[Int]("genre_id", O.Unique, O.PrimaryKey, O.AutoInc)
+            def name = column[String]("name")
+
+            def * = (id, name)
           }
+          val genres = TableQuery[Genres]
+
+          final class Summary(tag: Tag) extends Table[(Int, String, String, String, String, String, Double, String)](tag, "summary") {
+            def id = column[Int]("summary_id", O.Unique, O.PrimaryKey, O.AutoInc)
+            def title = column[String]("title")
+            def img = column[String]("img")
+            def link = column[String]("link", O.Unique, O.Length(200))
+            def desc = column[String]("desc")
+            def studio = column[String]("studio")
+            def rating = column[Double]("rating")
+            def showType = column[String]("showType")
+
+            def * = (id, title, img, link, desc, studio, rating, showType)
+          }
+          val summary = TableQuery[Summary]
+
+          final class List(tag: Tag) extends Table[(Int, Int, Int)](tag, "list") {
+            def id = column[Int]("id", O.Unique, O.PrimaryKey, O.AutoInc)
+            def sumId = column[Int]("sum_id")
+            def genId = column[Int]("gen_id")
+
+            def * = (id, sumId, genId)
+          }
+          val list = TableQuery[List]
+
+          val db = Database.forURL("jdbc:postgresql://localhost:5432/postgres", driver = "org.postgresql.Driver", user = "alice", password = "pass")
+
+          identity {
+            val tables = List(genres, summary, list)
+
+            tables.par.foreach{a =>
+              util.Try(db.run(DBIO.seq(a.schema.create)).v) match {
+                case Failure(e) => println(s"Could not create db ${e.getMessage}")
+                case _ => println("Successfully created db")
+              }
+            }
+
+            true
+          } && identity {
+            val genresList = db.run(genres.result.map(_.map(a => a._2))).v.toSet
+            val genreList = itemCache.cache.values.flatMap(_.getSummary.genres).toSet
+
+            val diff = genreList.diff(genresList)
+            db.run(DBIO.seq(
+              genres ++= diff.map((0, _))
+            )).v
+            true
+          } && identity {
+            val summaries = db.run(summary.result.map(_.map(a => (0, a._2, a._3, a._4, a._5, a._6, a._7, a._8)))).v.toSet
+            val summaries2 = itemCache.cache.values.map(_.getSummary).map(a => (0, a.title, a.img, a.link, a.desc, a.studio, a.rating, a.showType)).toSet
+            val diff = summaries2.diff(summaries)
+
+            db.run(DBIO.seq(
+              summary ++= diff
+            )).v
+            true
+          } && identity {
+            val genresList = db.run(genres.result.map(_.map(a => a._2 -> a._1))).v.toMap
+            val summaries = db.run(summary.result.map(_.map(a => a._4 -> a._1))).v.toMap
+
+            val sumGen = db.run(list.result.map(_.map(a => (a._2, a._3)))).v.toSet
+            val sumGen2 = itemCache.cache.values.map(_.getSummary).flatMap(a => a.genres.map(b => (summaries(a.link), genresList(b)))).toSet
+            val diff = sumGen2.diff(sumGen)
+            db.run(DBIO.seq(
+              list ++= diff.map(a => (0, a._1, a._2))
+            )).v
+            true
+          }
+
+          db.close()
+          true
         }
 
-        true
-      } && identity {
-        val genresList = db.run(genres.result.map(_.map(a => a._2))).v.toSet
-        val genreList = itemCache.cache.values.flatMap(_.getSummary.genres).toSet
-
-        val diff = genreList.diff(genresList)
-        db.run(DBIO.seq(
-          genres ++= diff.map((0, _))
-        )).v
-        true
-      } && identity {
-        val summaries = db.run(summary.result.map(_.map(a => (0, a._2, a._3, a._4, a._5, a._6, a._7, a._8)))).v.toSet
-        val summaries2 = itemCache.cache.values.map(_.getSummary).map(a => (0, a.title, a.img, a.link, a.desc, a.studio, a.rating, a.showType)).toSet
-        val diff = summaries2.diff(summaries)
-
-        db.run(DBIO.seq(
-          summary ++= diff
-        )).v
-        true
-      } && identity {
-        val genresList = db.run(genres.result.map(_.map(a => a._2 -> a._1))).v.toMap
-        val summaries = db.run(summary.result.map(_.map(a => a._4 -> a._1))).v.toMap
-
-        val sumGen = db.run(list.result.map(_.map(a => (a._2, a._3)))).v.toSet
-        val sumGen2 = itemCache.cache.values.map(_.getSummary).flatMap(a => a.genres.map(b => (summaries(a.link), genresList(b)))).toSet
-        val diff = sumGen2.diff(sumGen)
-        db.run(DBIO.seq(
-          list ++= diff.map(a => (0, a._1, a._2))
-        )).v
+        scribe.info("done")
         true
       }
-
-      db.close()
-      true
-    }
-
-    scribe.info("done")
+    )
   }
 }
